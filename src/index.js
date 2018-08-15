ed1ab5273dfe223301f8907d148a4678c178cf79
blermyjake <jeremy73120@gmail.com>
2018-08-15 17:13:27 -0500

    1-10

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
+
+  document.getElementById("root")
+);
 registerServiceWorker();
-
